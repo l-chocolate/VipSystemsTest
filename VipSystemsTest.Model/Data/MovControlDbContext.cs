36e52821116aa361e3bc209b20e7f8cddf068970
Lucas de Oliveira do Nascimento <lu-nascimento@live.com>
2024-03-05 17:16:02 -0300

    Adiciona o EntityFramework e o DbContext

---
+++ VipSystemsTest.Model/Data/MovControlDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VipSystemsTest.Model.Entities;
+
+namespace VipSystemsTest.Model.Data
+{
+    internal class MovControlDbContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Movimento> Movimentos { get; set; }
+
+        public MovControlDbContext(DbContextOptions<MovControlDbContext> options) : base(options)
+        {
+        }
+    }
+}
